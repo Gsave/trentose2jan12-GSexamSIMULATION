240faac4194f14ab2d3c73f16a321f1f51ca2282
Gianmaria Savegnago <gianmaria.savegnago@studenti.unitn.it>
2019-01-15 15:47:23 +0100

    little error fixed again

--- area_routes.js
+++ area_routes.js
@@ -5,7 +5,7 @@ const areaRoutes = express.Router();
 
 areaRoutes.route('/')
 .get(function (req, res) {
-    var sides = [req.param.side1,req.param.side2]
+    var sides = [req.query.side1,req.query.side2]
     var result = area.gerArea(sides)
     if(result == -1){
         res.status(400)
