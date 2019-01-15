de9a165ff0838f84c9d5a9a2eab12442fe693638
Gianmaria Savegnago <gianmaria.savegnago@studenti.unitn.it>
2019-01-15 15:29:10 +0100

    aggiunta funzione che controlla se intero, e modificata funzione getArea

--- area.js
+++ area.js
@@ -1,7 +1,15 @@
 class Area{
+    static isInt(n){
+        if (is_numeric(n) && (n%1 ==0)) {
+            return true
+        }else{
+            return false
+        }
+    }
+
     static getArea(sides){
         var ris 
-        if( (typeof sides[0] == "integer") && (typeof sides[1] == "integer")){
+        if( isInt(sides[0]) && sides[0]>0 && isInt(sides[1]) && sides[1]>0){
             ris = sides[0]*sides[1]
         }else{
             ris = -1
